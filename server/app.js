4669e1b6ec3eaa24e892242cc1e05cd2f3a65a6b
Metazoans <john3620@naver.com>
2024-12-10 22:00:59 +0900

    김선용 README.md 작성, GITHUB 병합 테스트 12/10

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config({ path : './database/mysql.env'});
+const express = require('express');
+const app = express();
+
+// 미들웨어
+app.use(express.json()); // application/json
+
+// 라우터 등록
+
+app.listen(3000, ()=>{
+  console.log('Server Start');
+  console.log('http://localhost:3000');
+})
+
+// 라우팅
+app.get('/', (req, res)=>{
+  res.send('Welcome!!');
+})
