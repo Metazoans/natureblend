0e19e9923f36ace4405cdbb6e888d7fd75e33d7b
SMGyo <alsryaks1@naver.com>
2024-12-12 15:30:20 +0900

    서민교 라우터 12/12

--- client/src/router/standardRoutes.js
+++ client/src/router/standardRoutes.js
@@ -1,2 +1,17 @@
 // 기준정보 라우터
 
+import StandardTest from '@/views/standard/StandardTest.vue';
+
+const standardRoutes = [
+  {
+    path: '',
+    redirect : { name : 'StandardTest'}
+  },
+  {
+    path: 'list',
+    name: 'standardTest',
+    component: StandardTest
+  },
+];
+
+export default standardRoutes;
---
+++ client/src/views/standard/StandardTest.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>dtrgohr</h1>
+    </div>
+</template>
