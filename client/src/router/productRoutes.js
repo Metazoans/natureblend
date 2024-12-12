6d6490fae50d3ebcb10b55b979f7ec7f2a46a18f
Metazoans <john3620@naver.com>
2024-12-12 15:26:53 +0900

    김선용 생산 vue 테스트 12/12

--- client/src/router/productRoutes.js
+++ client/src/router/productRoutes.js
@@ -1,1 +1,17 @@
 // 생산 라우터
+
+import ProductList from '@/views/product/ProductList.vue';
+
+const productRoutes = [
+  {
+    path: '',
+    redirect : { name : 'productList'}
+  },
+  {
+    path: 'list',
+    name: 'productList',
+    component: ProductList
+  },
+];
+
+export default productRoutes;
