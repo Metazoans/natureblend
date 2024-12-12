961daef6638b82ba5f5fd95c1494565f36966f2b
MyBFMoti <jiny5388@gmail.com>
2024-12-12 10:04:30 +0900

    new file:   server/appTest.js 	new file:   server/database/mapper.js 	new file:   server/database/sql.js 	new file:   server/database/sqls/boards.js 	new file:   server/router/board_router.js 	new file:   server/router/practice_router.js 	new file:   server/service/board_service.js

---
+++ server/database/mapper.js
@@ -0,0 +1,47 @@
+const mysql = require('mysql');
+//const mariadb = require('mariadb/callback');
+const sqlList = require('./sql.js');
+
+const connectionPool = mysql.createPool({
+//const connectionPool = mariadb.createPool({
+  host : process.env.MYSQL_HOST,
+  port : process.env.MYSQL_PORT,
+  user : process.env.MYSQL_USER,
+  password : process.env.MYSQL_PWD,
+  database : process.env.MYSQL_DB,
+  connectionLimit : process.env.MYSQL_LIMIT,
+
+  trace : true, //log
+  permitSetMultiParamEntries : true, //parameter가 객체일 경우 escape 작업
+  insertIdAsNumber : true,           //insertId를 Number 타입으로
+  bigIntAsNumber : true,             //bigInt를 자동으로 Number 타입으로
+});
+
+const query = (alias, values)=>{
+  return new Promise((resolve, reject)=>{
+    let executeSql = sqlList[alias];
+    connectionPool.query(executeSql, values, (err, results)=>{
+      if(err) {
+        reject({err});
+      }else{
+        resolve(results);
+      }
+    });
+  });
+};
+
+// const query = (alias, values)=>{
+//   let executeSql = sqlList[alias];
+//   let conn;
+//   try{
+//     const res = await conn.query('select 1', [2]);
+//     console.log(res); // [{ "1": 1 }]
+//     return res;
+//   }catch(err){
+//     console.log
+//   } 
+// };
+
+module.exports = {
+  query,
+}
