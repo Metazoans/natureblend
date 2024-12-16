49161e6095989b2f57fd4da25a29538caf2afe42
MyBFMoti <jiny5388@gmail.com>
2024-12-16 17:55:33 +0900

    이신화 sql.js수정(quality) 12/16

--- server/database/sql.js
+++ server/database/sql.js
@@ -1,17 +1,19 @@
 const boards = require('./sqls/boards.js');
 const comments = require('./sqls/comments.js');
 const books = require('./sqls/books.js');
-const quality = require('./sqls/quality.js');
-const emp = require('./sqls/emp.js');
 const material = require('./sqls/material.js');
 const productionPlan = require('./sqls/production/productionPlan')
+const emp = require('./sqls/emp.js')
+const bomlist = require('./sqls/standard/bomlist.js');
+const quality = require('./sqls/quality.js');
 
 module.exports = {
   ...boards,
   ...comments,
   ...books,
   ...emp,
-  ...quality,
   ...material,
   ...productionPlan,
+  ...bomlist,
+  ...quality,
 }
