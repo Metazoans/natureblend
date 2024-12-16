ca08e8b20878a060bcec6a2838ce74de4454b333
yunngna <nayunkwon124@gmail.com>
2024-12-16 11:13:12 +0900

    권나윤_메인분기병합1216

--- server/service/sales_service.js
+++ server/service/sales_service.js
@@ -7,14 +7,14 @@ const getClientList = async ()=>{
 }
 // 검색 기준 전체조회
 const getOrderList = async (orderStatus, orderName, clientName, startDate, endDate)=>{
-    let searchInfo = [
-        orderStatus ? `%${orderStatus}%` : '' ,orderStatus,
-        orderName ? `%${orderName}%` : '' , orderName, 
-        clientName ? `%${clientName}%` : '' , clientName, 
-        startDate ? startDate : '', 
-        endDate ? endDate : '',
-    ];
-    let result = await mysql.query('orderList',searchInfo);
+    // let searchInfo = [
+    //     orderStatus ? `%${orderStatus}%` : '' ,orderStatus,
+    //     orderName ? `%${orderName}%` : '' , orderName, 
+    //     clientName ? `%${clientName}%` : '' , clientName, 
+    //     startDate ? startDate : '', 
+    //     endDate ? endDate : '',
+    // ];
+    let result = await mysql.query('orderList',orderStatus,orderStatus,orderName,orderName);
     return result;
 }
 
