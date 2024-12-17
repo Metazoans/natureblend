0f8c062667c6e5da5fd8ce400498a4b1fe398882
Metazoans <john3620@naver.com>
2024-12-17 11:01:54 +0900

    김선용 server app.js 정리 12/17

--- server/app.js
+++ server/app.js
@@ -1,36 +1,57 @@
 require('dotenv').config({ path : './database/mysql.env'});
 const express = require('express');
 const app = express();
-const productionRouter = require('./router/production_router')
 const boardRouter = require('./router/board_router.js');
 const bookRouter = require('./router/book_router.js');
-const qualityRouter = require('./router/qc_router.js');
 const empRouter = require('./router/emp_router.js');
+
+// 영업
 const salesRouter = require('./router/sales_router.js');
+
+// 자재
 const materialRouter = require('./router/material_router.js');
-const standardRouter = require('./router/standard_router.js');
+
+// 생산
+const productionRouter = require('./router/production_router');
+
+// 품질
+const qualityRouter = require('./router/qc_router.js');
 
 // 설비
 const machineRouter = require('./router/machine/machine_router.js');
 const inactRouter = require('./router/machine/inact_router.js');
 
+// 기준정보
+const standardRouter = require('./router/standard_router.js');
+
 // 미들웨어
 app.use(express.json()); // application/json
-app.use(express.urlencoded({ extended : false}))
+app.use(express.urlencoded({ extended : false}));
 
 // 라우터 등록
-app.use('/production', productionRouter);
 app.use('/', boardRouter);
 app.use('/', bookRouter);
 app.use('/', empRouter);
-app.use('/', qualityRouter);
-app.use('/', salesRouter);
-app.use('/', materialRouter);
-app.use('/', standardRouter);
 
-// 설비
+// 영업 라우터
+app.use('/', salesRouter);
+
+// 자재 라우터
+app.use('/', materialRouter);
+
+// 생산 라우터
+app.use('/production', productionRouter);
+
+// 품질 라우터
+app.use('/', qualityRouter);
+
+// 설비 라우터
 app.use('/machine', machineRouter);
 app.use('/inActs', inactRouter);
+
+// 기준정보 라우터
+app.use('/', standardRouter);
+
 
 app.listen(3000, ()=>{
   console.log('Server Start');
@@ -38,29 +59,7 @@ app.listen(3000, ()=>{
 })
 
 
-
-// require('dotenv').config({ path : './database/mysql.env'});
-// const express = require('express');
-// const app = express();
-// const boardRouter = require('./router/board_router.js');
-// const bookRouter = require('./router/book_router.js');
-// const empRouter = require('./router/emp_router.js')
-//
-// // 미들웨어
-// app.use(express.json()); // application/json
-// app.use(express.urlencoded({ extended : false})) // application/x-form-urlencoded
-//
-// // 라우터 등록
-// app.use('/', boardRouter);
-// app.use('/', bookRouter);
-// app.use('/', empRouter);
-//
-// app.listen(3000, ()=>{
-//   console.log('Server Start');
-//   console.log('http://localhost:3000');
-// })
-//
-// // 라우팅
+// 라우팅
 app.get('/', (req, res)=>{
   res.send('Welcome!!');
 })
