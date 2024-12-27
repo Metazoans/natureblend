189252316fe1fd37b85d5e5d1a20a86efc24f42e
Dayoung <kimdayoung771@gmail.com>
2024-12-27 17:53:24 +0900

    김다영 생산계획 검색 작업중 12/27

--- server/service/production/plan_service.js
+++ server/service/production/plan_service.js
@@ -27,7 +27,18 @@ const planList = async (urlQuery)=>{
     if(Object.keys(urlQuery).length === 0) {
         return await mysql.query('planDetailList');
     } else {
-        // let dbQuery =
+        // query {
+        //     productCode: 'P002',
+        //         status: 'plan_in_process,plan_complete',
+        //         startDate: '2024-12-14',
+        //         endDate: '2025-01-07'
+        // }
+
+        // let dbQuery = ''
+        // if(urlQuery.productCode) {
+        //     dbQuery += ` and product_code = ${urlQuery.productCode}`
+        // }
+
     }
 }
 
