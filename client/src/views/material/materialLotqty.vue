22f918be99dcf3b11dbce8856addb4d50241e11c
hayeon8521 <hayeon8521@kakao.com>
2025-01-08 15:20:20 +0900

    표하연 진짜 최종 01/08

--- client/src/views/material/materialLotqty.vue
+++ client/src/views/material/materialLotqty.vue
@@ -104,7 +104,7 @@
       :paginationPageSize="9"
       :paginationPageSizeSelector="[9, 20, 50, 100]"
       @grid-ready="onReady"
-      style="height: 473px;"
+      style="height: 471px;"
       rowSelection="multiple"
       :noRowsOverlayComponent="CustomNoRowsOverlay"
       :tooltipShowDelay="500"
@@ -230,26 +230,26 @@ const loginInfo = () => {
  
   //그리드 api 컬럼명 들어가는 거
  const columnDefs = ref([
-   { headerName: "No.", field: "lot_seq", width:90, cellStyle: { textAlign: "center" }, tooltipField: "lot_seq", },
-   { headerName: "자재 LOT번호", field: "lot_code", width:160, cellStyle: { textAlign: "center" } },
-   { headerName: "자재명", field: "material_name", cellStyle: { textAlign: "left" } },
-   { headerName: "업체명", field: "com_name", cellStyle: { textAlign: "left" } },
-   { headerName: "입고량", field: "in_qty", width:130, cellStyle: { textAlign: "right" } },
-   { headerName: "입고담당", field: "name", width:120, cellStyle: { textAlign: "center" } },
-   { headerName: "가용", field: "material_nomal", width:85, cellStyle: { textAlign: "center" } },
-   { headerName: "현재고", field: "stok_qty", width:130, cellStyle: { textAlign: "right" } },
-   { headerName: "불가용", field: "hold_qty", width:130, cellStyle: { textAlign: "right" } },
-   { headerName: "출고재고", field: "out_qty", width:130, cellStyle: { textAlign: "right" } },
-   { headerName: "입고일", field: "inset_lot_date", width:130, cellStyle: { textAlign: "center" } },
-   { headerName: "유통기한", field: "limit_date", width:130, cellStyle: { textAlign: "center" } },
-   { headerName: "창고위치", field: "warehouse_name", width:120, cellStyle: { textAlign: "left" } },
-   { headerName: "폐기", field: "material_lot_state", width:80, cellStyle: { textAlign: "center" },
+   { headerName: "No.", field: "lot_seq", flex:2, cellStyle: { textAlign: "center" }, tooltipField: "lot_seq", },
+   { headerName: "자재 LOT번호", field: "lot_code", flex:4, cellStyle: { textAlign: "center" } },
+   { headerName: "자재명", field: "material_name", flex:4, cellStyle: { textAlign: "left" } },
+   { headerName: "업체명", field: "com_name", flex:4, cellStyle: { textAlign: "left" } },
+   { headerName: "입고량", field: "in_qty", flex:3, cellStyle: { textAlign: "right" } },
+   { headerName: "입고담당", field: "name", flex:3, cellStyle: { textAlign: "center" } },
+   { headerName: "가용", field: "material_nomal", flex:3, cellStyle: { textAlign: "center" } },
+   { headerName: "현재고", field: "stok_qty", flex:3, cellStyle: { textAlign: "right" } },
+   { headerName: "불가용", field: "hold_qty", flex:3, cellStyle: { textAlign: "right" } },
+   { headerName: "출고재고", field: "out_qty", flex:3, cellStyle: { textAlign: "right" } },
+   { headerName: "입고일", field: "inset_lot_date", flex:3, cellStyle: { textAlign: "center" } },
+   { headerName: "유통기한", field: "limit_date", flex:3, cellStyle: { textAlign: "center" } },
+   { headerName: "창고위치", field: "warehouse_name", flex:3, cellStyle: { textAlign: "left" } },
+   { headerName: "폐기", field: "material_lot_state", flex:2, cellStyle: { textAlign: "center" },
       tooltipValueGetter: params => params.data.material_dis_content ? `담당 : ${params.data.dis_name} / 사유 : ${params.data.material_dis_content}`: "",  
    },
    {  
       headerName: "비고", 
       field: "폐기처리", 
-      width:100,
+      flex:2,
       editable: false,
       cellRenderer: params => {
          if(params.data.material_lot_state === "정상" && (loginfo.value.job === '자재' || loginfo.value.job === '관리자') ){
@@ -369,7 +369,8 @@ const onExportClick = () => {
  };
  
  const onReady = (param) => {
-   param.api.sizeColumnsToFit(); //그리드 api 넓이 슬라이드 안생기게하는거
+   //param.api.sizeColumnsToFit(); //그리드 api 넓이 슬라이드 안생기게하는거
+   console.log('지금은 안씀', param);
  }
  
  //데이터 잡아넣을 그리드 api 행
